1b63c5b26f4d518808d4e32bf489d059e6bde5b6
Wadih <Wadih@Wadih-PC>
2014-07-15 21:32:53 +0300

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	web/index.html

---
+++ error.php
@@ -0,0 +1,20 @@
+<?php
+    require_once $_SERVER["DOCUMENT_ROOT"]."/resources/header.inc.php";
+    if(!isset($_GET["err"])){
+        header("location:/index.php");
+    }
+    $errorMsg = array(
+        1=>"Vous n'avez pas l'authorization d'accédez à cette page",
+        2=>"Erreur 2"
+    );
+?>
+
+    <div class="imgError">
+        <img src="resources/img/error.jpg" alt="" style="margin: 8% auto auto 38%;"/>
+    </div>
+    <div class="errorMsg">
+        <?= $errorMsg[$_GET["err"]] ?>
+    </div>
+<?php
+    require_once $_SERVER["DOCUMENT_ROOT"]."/resources/footer.inc.php";
+?>
--- user/add.php
+++ user/add.php
@@ -1,6 +1,6 @@
 <?php
+    $acl = 1;
     require_once $_SERVER["DOCUMENT_ROOT"]."/resources/header.inc.php";
-    print_r($_SESSION["user"]);
 ?>
 <?php
     require_once $_SERVER["DOCUMENT_ROOT"]."/resources/footer.inc.php";
