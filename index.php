693e584e5cecf49cfad4aeb9524411c42ecf0e27
Hala El Hage <Hala El Hage@LENOVO-PC>
2014-07-15 21:23:32 +0300

    Merge origin/master
    Conflicts:
    	nbproject/project.propertieshhjj

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
     $acl = 4;
     require_once $_SERVER["DOCUMENT_ROOT"]."/resources/header.inc.php";
 ?>
+<div class="main">
+    
+</div>
 <?php
     require_once $_SERVER["DOCUMENT_ROOT"]."/resources/footer.inc.php";
 ?>
