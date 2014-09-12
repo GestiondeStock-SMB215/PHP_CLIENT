43850d62178fc9ebad4a3d75eb5c17cde163349b
Wadih <Wadih@Wadih-PC>
2014-09-12 21:40:38 +0300

    Ajout de la page show.php pour les customers et les suppliers Commit sur la fonction delete des branches il reste un bug à rectifier

--- cat/show.php
+++ cat/show.php
@@ -1,53 +1,57 @@
-<style>
-    .global{width:400px;}
-    
-    .name{float:left;text-indent:50px;border:1px solid black;width:170px;font-size:18px;font-weight:bold}
-    
-    .desc{float:right;text-indent:60px;border:1px solid black;width:226px;font-size:18px;font-weight:bold}
-     
-    #global{height:150px;width:400px}
-            
-    #name{width:170px;border:1px solid black;}
-    
-    #desc{float:right;border:1px solid black;text-indent:60px;width:226px;height:20px;margin-top:-22px;}    
-</style>
-    <script>
-    $("#check").click(function () {
-        if($("#check").checked){
-       deleteCategory();
-   }
-    });
-    </script>
 <?php
-    require_once $_SERVER["DOCUMENT_ROOT"]."/resources/header.inc.php";
-    
-function getCategories(){
-        global $wsdl;
-        set_time_limit(0);
-        $response = $wsdl->getCategories();
-        echo "<div class='registerContainer' id='example'>".
-             "<h3>CATEGORIES</h3>".
-             "<div class='global'>".
-             "<div class='name'>NAME</div>".
-             "<div class='desc'>DESCRIPTION</div>";
-        foreach($response as $result){
+require_once $_SERVER["DOCUMENT_ROOT"]."/resources/header.inc.php";
+?>
+<script>
+$(document).ready(function() {
+    $('#example').dataTable({
+        "iDisplayLength":5  
+    });
+});
+</script>
+<div class="clear"></div>
+<table width="100%"><tr><td><h3>Category Management</h3></td><td align="right"><input type="button" value="Create" class="myButton" onclick="javascript:window.location.href='add.php'"/></td></tr></table>
+<table id="example" class="display cell-border">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>DESCRIPTION</th>
+            <th>PIC</th>
+            <th>Time Stamp</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            $objs = getCategories();
             
-            foreach($result as $item){
-                
-                echo "<div id='global'>".  
-                     "<div id='name'><input id='check' type='checkbox'/>$item->cat_name</div>".
-                     "<div id='desc'>$item->cat_desc</div>";
-               
+            foreach($objs as $obj){
+                echo "<tr>";
+                    echo "<td>".$obj["cat_id"]."</td>";
+                    echo "<td>".$obj["cat_name"]."</td>";
+                    echo "<td>".$obj["cat_desc"]."</td>";
+                    echo "<td>".$obj["cat_pic"]."</td>";
+                    echo "<td>".$obj["cat_time_stamp"]."</td>";
+                echo "<td><a href=\"edit.php?bra_id=".$obj["cat_id"].""."\">Edit</a></td>";
+                echo "<td><a href=\"delete.php?bra_id=".$obj["cat_id"]."\">Delete</a></td>";
+                echo "</tr>";
             }
-        }
-        echo "</div></div></div>";
-    }
-    function deleteCategory(){
-        global $wsdl;
-        set_time_limit(0);
-        $response = $wsdl->deleteCategory();
-    
-    }
-    ?>
-   <?php getCategories(); ?>
-   
+        ?>
+    </tbody>
+    <tfoot>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>DESCRIPTION</th>
+            <th>PIC</th>
+            <th>Time Stamp</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </tfoot>    
+</table>
+
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"]."/resources/footer.inc.php";
+?>
