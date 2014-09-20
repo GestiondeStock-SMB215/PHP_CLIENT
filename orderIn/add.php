0f7b106a7c962fa301f084366c699183373590fd
Hala El Hage <Hala El Hage@LENOVO-PC>
2014-09-20 15:57:21 +0300

    Order In Pages and Format

--- orderIn/add.php
+++ orderIn/add.php
@@ -6,11 +6,26 @@ require_once $_SERVER["DOCUMENT_ROOT"]."/resources/header.inc.php";
 <link href="../resources/css/jquery.ui.datepicker.css" rel="stylesheet" /> 
 
 <div class="orderInContainer">
-    <div class="txtInputDate">
-        <div class="lbl">Date Time</div>  
-        <input tye="text" class="inputDate" id="orderInDate" />   
-        <div class="clear"></div>
-        <div class="infoHolder dateTime" style="display:none;float:right;">
+    <div class="title">Order In </div>
+    <div class="headOrderIn">
+        <div class="lbl">Date Time</div> 
+        <div class="lbl">Product</div> 
+        <div class="lbl">Description</div>
+        <div class="lbl">Quantity</div>
+        <div class="lbl">Price</div>
+        <div class="lbl">Total</div>
+    </div>
+    <div class="clear"></div>
+    <div class="orderInContent">
+        <input type="text" class="inputDate" id="orderInDate" />
+        <select id="" class="lblInput" type="text" >
+            <option>Select Product</option>
+        </select>
+        <input id="" class="lblInput" type="text" disabled="disabled" />
+        <input id="" class="lblInput" type="text" />
+        <input id="" class="lblInput" type="text" disabled="disabled" />
+        <input id="" class="lblInput" type="text" disabled="disabled" />
+        <div class="infoHolder dateTime" style="display:none;float:left;">
             <div class="infoHolderContent">
                 <div id="datepicker"></div>
                 <div class="calendarInfoBox"></div>
@@ -18,27 +33,7 @@ require_once $_SERVER["DOCUMENT_ROOT"]."/resources/header.inc.php";
         </div>
         <div class="calendarTxt" style="display: none;"></div>  
     </div>
-    <div class="clear"></div>
-    <div class="txtInput">
-        <div class="lbl">Product</div>
-        <input id="" class="lblInput" type="text" />
-    </div>
-    <div class="txtInput">
-        <div class="lbl">Description</div>
-        <input id="" class="lblInput" type="text" />
-    </div>
-    <div class="txtInput">
-        <div class="lbl">Quantity</div>
-        <input id="" class="lblInput" type="text" />
-    </div>
-    <div class="txtInput">
-        <div class="lbl">Price</div>
-        <input id="" class="lblInput" type="text" />
-    </div>
-    <div class="txtInput">
-        <div class="lbl">Total</div>
-        <input id="" class="lblInput" type="text" />
-    </div>
+ 
     <div class="txtInput" style="height:100px;">
         <input id="" class="btnRegister" name="Back" type="button" value="Cancel" onclick="javascript=window.location.href='show.php'" />        
         <input id="btnAdd" class="btnRegister" type="button" type="button" value="Add Product" />
@@ -49,8 +44,8 @@ require_once $_SERVER["DOCUMENT_ROOT"]."/resources/header.inc.php";
 
 <script type="text/javascript">
     $(document).ready(function (){
-        $(".calendarTxt").html($.datepicker.formatDate('dd/MM/yy', new Date()));
-        $("#orderInDate").val($.datepicker.formatDate("dd/MM/yy", new Date()));
+        $(".calendarTxt").html($.datepicker.formatDate('yy-MM-dd', new Date()));
+        $("#orderInDate").val($.datepicker.formatDate("yy-MM-dd", new Date()));
 
         $("#datepicker").datepicker({ prevText: '<', nextText: '>', minDate: 0, autoSize: true, onChangeMonthYear: function () { $(".calendarInfoBox").hide() }, onSelect: function (dateText, inst) {
             dd = dateText;
@@ -64,8 +59,8 @@ require_once $_SERVER["DOCUMENT_ROOT"]."/resources/header.inc.php";
             var d = $(".calendarInfoBox");
             d.css("position", 'absolute');
             var position = $(".ui-state-active").parent().position();
-            $(".calendarTxt").html($.datepicker.formatDate("dd/MM/yy", new Date(dd)));
-             $("#orderInDate").val($.datepicker.formatDate("dd/MM/yy", new Date(dd)));
+            $(".calendarTxt").html($.datepicker.formatDate("yy-MM-dd", new Date(dd)));
+             $("#orderInDate").val($.datepicker.formatDate("yy-MM-dd", new Date(dd)));
             $(".calendarInfoBox").css("left", (position.left + 35) + 'px');
             $(".calendarInfoBox").css("top", (position.top - 7) + 'px');
             $("#dateLanguage").html(ddd);
@@ -76,7 +71,7 @@ require_once $_SERVER["DOCUMENT_ROOT"]."/resources/header.inc.php";
         });
         
          $("#btnAdd").click(function(){
-            
+          
         });
     });
      
