c83bf51d7780bbef856ec69c3c4d933ececec1a4
Henry Kozhaya <henry.kozhaya@gmail.com>
2014-10-16 15:36:48 +0300

    Security added to the transfert User and Manager can not change the source branch Admin can change the source branch

--- invoiceIn/add.php
+++ invoiceIn/add.php
@@ -1,8 +1,12 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"]."/resources/header.inc.php";
+if(!isset($_GET["ord_in_id"])){
+    header("location:show.php");
+}
 
 getProdSession();
 getCustSession();
+
 ?>
 
 <?php
